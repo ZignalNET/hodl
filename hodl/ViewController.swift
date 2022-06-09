563d77c1ad7a68c37b38cccbb9c8cefa7ae5008b
ZignalNET <emmanuel@zignal.net>
2022-06-09 08:11:51 +0200

    Initial Commit

---
+++ hodl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hodl
+//
+//  Created by Emmanuel Adigun on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
